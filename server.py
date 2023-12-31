d2d254f88cf62e6f84ea22ca6d9d47892929624e
lozardo <109414676+lozardo@users.noreply.github.com>
2023-12-31 11:12:47 +0200

    Update server.py
    this works

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-# server.py
 import socket
 import threading
 import json
@@ -6,38 +5,49 @@ import pickle
 
 # Store user information including colors
 user_colors = {}
+connected_clients = []  # List to store connected clients
+lock = threading.Lock()
+
+def send_to_all_clients(message):
+    with lock:
+        print(message)
+        for client_socket, client_addr in connected_clients:
+            try:
+                client_socket.sendall(pickle.dumps(message))
+            except Exception as e:
+                print(f"Error sending message to {client_addr}: {e}")
 
 def handle_client(client_socket, addr):
     while True:
+        #try:
+        data = client_socket.recv(1024)
+        message = pickle.loads(data)
+        print(f"{addr}: {message}")
+        message_type = message[0]
 
-        try:
-            data = client_socket.recv(1024)
-            message = pickle.loads(data)
-            print(f"{addr}:{message}")
-            message_type = message[0]
+        if message_type == "join":
+            # Assign a color to the user
+            user_colors[str(addr)] = (0, 0, 0)  # Convert tuple to string
+        elif message_type == "color":
+            user_colors[str(addr)] = message[1]  # Convert tuple to string
+        elif message_type == "draw":
+            # Print the message to debug
 
-            if message_type == "join":
-                # Assign a color to the user
-                user_colors[addr] = (0, 0, 0)  # Initial color: black
-            elif message_type == "color":
-                user_colors[addr] = message[1]
-            elif message_type == "draw":
-                # Broadcast drawing updates to other clients
-                color = user_colors[addr]
-                broadcast_message = {"type": "drawing", "data": {"points": message["data"]["points"], "color": color}}
+            # Broadcast drawing updates to other clients
+            color = user_colors[str(addr)]  # Convert tuple to string
+            broadcast_message = {"type": "drawing", "data": [message[1], color]}
 
-                for client_address in user_colors:
-                    if client_address != addr:
-                        client_socket.sendto(json.dumps(broadcast_message).encode(), client_address)
-
-        except Exception as e:
+            send_to_all_clients(broadcast_message)
+        '''except Exception as e:
             print(f"Error handling client {addr}: {e}")
-            break
+            break'''
 
     # Remove user when they disconnect
-    print(user_colors[addr])
-    del user_colors[addr]
+    print(user_colors[str(addr)])  # Convert tuple to string
+    del user_colors[str(addr)]  # Convert tuple to string
     client_socket.close()
+
+    # Remove user when they
 
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
