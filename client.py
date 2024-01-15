e6803cb6293a165deb7d9424bb52d130d3fc34cd
lozardo <109414676+lozardo@users.noreply.github.com>
2024-01-15 15:08:37 +0200

    Update client.py - not working
    read client

--- client.py
+++ client.py
@@ -1,148 +1,96 @@
-
-import pickle
+import os
 import socket
-import threading
-from tkinter import filedialog  # for saving image
-
-import pygame
-import sys
+import struct
 
 import pygame_widgets
+
+from white_lib import WhiteboardApp
+import pickle
+import pygame
+import sys
+import threading
 from pygame_widgets.slider import Slider
 from pygame_widgets.button import Button
 
 
-class WhiteboardApp:
-    def __init__(self, width, height):
+class ClientApp(WhiteboardApp):
+    def __init__(self):
+        # Connect to the server
+        server_address = ("127.0.0.1", 5555)
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect(("127.0.0.1", 5555))
-
-        pygame.init()
-
-        self.width = width
-        self.height = height
-
-        self.screen = pygame.display.set_mode((width, height))
-        pygame.display.set_caption("Whiteboard App")
-
-        self.clock = pygame.time.Clock()
-
-        self.setup()
+        self.client_socket.connect(server_address)
+        self.initialize(True)
 
     def receive_messages(self):
         self.client_socket.settimeout(0.1)
         while True:
-            print("b")
-            try:
-                data = self.client_socket.recv(1024)
-                print("a")
-                if data:
-                    message = pickle.loads(data)
-                    if message[0] == 'drawing':
-                        self.draw(message[1][0], message[1][1], message[1][2], message[1][3])
-                    print(f"Received message from server: {message}")
-            except Exception as e:
-                print(f"Error receiving data from server: {e}")
+            #try:
+            # Receive the size of the incoming data
+            size_data = self.client_socket.recv(4)
+            if not size_data:
+                break
+            data_size = struct.unpack("!I", size_data)[0]
 
-    def setup(self):
-        self.line_color_index = 0
-        self.available_colors = [
-            [(0, 0, 0), (20, 20, 20)],    # Black
-            [(255, 0, 0), (240, 0, 0)],  # Red
-            [(0, 255, 0), (0, 240, 0)],  # Green
-            [(0, 0, 255), (0, 0, 240)],  # Blue
-            [(128, 0, 128), (113, 0, 113)],  # Purple
-            [(139, 69, 19), (124, 54, 4)],   # Brown
-            [(0, 128, 0), (0, 113, 0)],   # Green
-            [(128, 128, 128), (113, 113, 113)],  # Gray
-            [(255, 255, 255), (240, 240, 240)]   # White
-        ]
-        self.draw_color = self.available_colors[self.line_color_index][0]
+            # Receive the actual data
+            data = b""
+            while len(data) < data_size:
+                packet = self.client_socket.recv(data_size - len(data))
+                if not packet:
+                    break
+                data += packet
 
-        self.line_width_options = [1, 4, 8, 12]
-        self.line_width_index = 0
-        self.line_width = self.line_width_options[self.line_width_index]
+            message = pickle.loads(data)
+            message_type = message[0]
 
-        self.drawing = False
-        self.points = []
-        self.last_circle_position = (0, 0)
+            if message_type == 'drawing':
+                self.draw(message[1][0], message[1][1], message[1][2], message[1][3])
+            elif message_type == 'initial_state':
+                self.initialize_whiteboard_with_image(message[1])
 
-        # Top toolbar
-        self.toolbar_height = 40
-        self.toolbar_color = (200, 200, 200)
-        self.color_buttons = []
+                print(f"Received message from server: {message}")
 
-        # Creating color buttons dynamically based on available colors
-        button_width = 40  # Set the width of each color button
-        for i, (inactive_color, active_color) in enumerate(self.available_colors):
-            color_button = Button(self.screen, i * button_width, 0, button_width, self.toolbar_height, text='',
-                                  fontSize=15, inactiveColour=inactive_color,
-                                  hoverColour=active_color, onClick=self.change_color, onClickParams=[i])
-            self.color_buttons.append(color_button)
+            #except Exception as e:
+            #    print(f"Error receiving data from server: {e}")
 
-        # Width slider
-        self.width_slider = Slider(self.screen, self.width - 200, 10, 150, 20, min=1, max=9, step=1,
-                                  initial=self.line_width_options[self.line_width_index], colour=(255, 255, 255),
-                                  handleColour=(0, 0, 0), handleRadius=10)
+    def initialize_whiteboard_with_image(self, image_data):
+        # Create a temporary file to save the received whiteboard image
+        temp_file_path = "temp_received_whiteboard.png"
 
-        # Bottom toolbar
-        self.bottom_toolbar_height = 40
-        self.save_button = Button(self.screen, self.width - 120, self.height - self.bottom_toolbar_height + 10,
-                                  100, 20, text='Save', fontSize=15, margin=20, inactiveColour=(255, 255, 255),
-                                  colour=(100, 100, 100), onClick=self.save_picture)
+        # Write the image data to the temporary file
+        with open(temp_file_path, "wb") as file:
+            file.write(image_data)
 
-    def change_color(self, args):
-        index = args
-        self.line_color_index = index
-        if self.draw_color != self.available_colors[self.line_color_index][0]:
-            self.draw_color = self.available_colors[self.line_color_index][0]
-            self.send_action("color", self.draw_color)
-            print(self.draw_color)
+        # Load the image and set it as the background
+        background_image = pygame.image.load(temp_file_path)
+        self.screen.blit(background_image, (0, 0))
+        pygame.display.flip()
 
-    def save_picture(self):
-        file_path = filedialog.asksaveasfilename(defaultextension=".png",
-                                                 filetypes=[("PNG files", "*.png"), ("All files", "*.*")])
+        # Remove the temporary file
+        pygame.time.wait(1000)  # Wait for 1 second to ensure the file is closed
+        pygame.display.flip()
+        os.remove(temp_file_path)
 
-        if file_path:
-            # Save only the region between the top and bottom toolbar
-            capture_rect = pygame.Rect(0, self.toolbar_height, self.width,
-                                       self.height - self.toolbar_height - self.bottom_toolbar_height)
-            captured_image = self.screen.subsurface(capture_rect)
-            pygame.image.save(captured_image, file_path)
-            print(f"Picture saved to {file_path}")
+    def update_image_with_chunk(self, chunk):
+        # Assuming self.image is a pygame.Surface representing your whiteboard image
+        if not hasattr(self, 'image'):
+            # Load the initial image when it's received
+            self.image = pygame.image.load(chunk)
+        else:
+            # Update the image with additional chunks
+            self.image.blit(pygame.image.load(chunk), (0, 0))
 
-    def draw_toolbar(self):
-        pygame.draw.rect(self.screen, self.toolbar_color, (0, 0, self.width, self.toolbar_height))
+        # Redraw the entire screen with the updated image
+        self.screen.fill((255, 255, 255))
+        self.screen.blit(self.image, (0, 0))
 
-        # Draw color buttons
-        for color_button in self.color_buttons:
-            color_button.draw()
-
-        # Draw width slider
-        self.width_slider.draw()
-
-    def draw_bottom_toolbar(self):
-        pygame.draw.rect(self.screen, self.toolbar_color,
-                         (0, self.height - self.bottom_toolbar_height, self.width, self.bottom_toolbar_height))
-
-        # Draw save button
-        self.save_button.draw()
+        pygame.display.flip()
 
     def send_action(self, action_type, data):
         drawing_action = (action_type, data)
         try:
             self.client_socket.sendall(pickle.dumps(drawing_action))
-        except socket.error as e:
-            print(f"Error sending data to server: {e}")
-
-    def draw(self, points, draw_color, line_width, last_circle_position):
-        for p in points:
-            pygame.draw.circle(self.screen, draw_color, p, line_width * 2)
-
-        if last_circle_position is not None:
-            pygame.draw.line(self.screen, draw_color,last_circle_position, points[-1],
-                             line_width * 5)
-
+        except:
+            print(f"Error sending data to server")
 
     def run(self):
         self.screen.fill((255, 255, 255))  # White background
@@ -177,7 +125,6 @@ class WhiteboardApp:
                         else:
                             self.points = [event.pos]
 
-
                 # Handle slider events
                 if self.line_width != int(self.width_slider.getValue()):
                     self.line_width = int(self.width_slider.getValue())
@@ -190,21 +137,18 @@ class WhiteboardApp:
             self.draw_bottom_toolbar()
 
             if len(self.points) > 0:
-
                 self.send_action("draw", (self.points, self.draw_color, self.line_width, self.last_circle_position))
                 print(self.points)
-                #self.draw(self.points, self.draw_color, self.line_width, self.last_circle_position)
+                # self.draw(self.points, self.draw_color, self.line_width, self.last_circle_position)
                 self.last_circle_position = self.points[-1]
 
             if not self.drawing:
                 self.last_circle_position = None
-
-
 
             pygame.display.flip()
             self.clock.tick(60)
 
 
 if __name__ == "__main__":
-    app = WhiteboardApp(800, 600)
+    app = ClientApp()
     app.run()
