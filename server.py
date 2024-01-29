b44494e8734950e22189bd1d2c11a5076861248c
lozardo <109414676+lozardo@users.noreply.github.com>
2024-01-29 15:12:26 +0200

    Update server.py - multiple whiteboards

--- server.py
+++ server.py
@@ -6,14 +6,14 @@ import pickle
 
 import white_lib
 
-connected_clients = []  # List to store connected clients
+connected_clients = {}  # Dictionary to store connected clients for each whiteboard
 lock = threading.Lock()
 
 
-def send_to_all_clients(message):
+def send_to_all_clients(message, whiteboard_id):
     with lock:
         print(message)
-        for client_socket, client_addr in connected_clients:
+        for client_socket, client_addr in connected_clients[whiteboard_id]:
             try:
                 print(f"{client_socket} - {client_addr}")
                 client_socket.sendall(pickle.dumps(message))
@@ -21,7 +21,8 @@ def send_to_all_clients(message):
                 print(f"Error sending message to {client_addr}: {e}")
 
 
-def handle_client(client_socket, addr, whiteboard):
+def handle_client(client_socket, addr):
+    whiteboard_id = -1
     while True:
         data = client_socket.recv(1024)
         message = pickle.loads(data)
@@ -29,25 +30,32 @@ def handle_client(client_socket, addr, whiteboard):
         message_type = message[0]
 
         if message_type == "join":
-            send_whiteboard_state_to_client(client_socket, whiteboard)
-            connected_clients.append((client_socket, addr))
+            whiteboard_id = message[1]
+            if whiteboard_id not in whiteboards:
+                whiteboards[whiteboard_id] = white_lib.WhiteboardApp()
+                whiteboards[whiteboard_id].initialize(False)
+                connected_clients[whiteboard_id] = []
+
+            send_whiteboard_state_to_client(client_socket, whiteboard_id)
+            connected_clients[whiteboard_id].append((client_socket, addr))
+
         elif message_type == "draw":
             # Broadcast drawing updates to other clients
             broadcast_message = ("drawing", message[1])
-            whiteboard.draw(message[1][0], message[1][1], message[1][2], message[1][3])
+            whiteboards[whiteboard_id].draw(message[1][0], message[1][1], message[1][2], message[1][3])
 
-            send_to_all_clients(broadcast_message)
+            send_to_all_clients(broadcast_message, whiteboard_id)
+
     # Remove user when they disconnect
-    connected_clients.remove((client_socket, addr))
-
+    connected_clients[whiteboard_id].remove((client_socket, addr))
     client_socket.close()
 
 
-
-def send_whiteboard_state_to_client(client_socket, whiteboard):
-    temp_file_path = "temp_whiteboard.png"
+def send_whiteboard_state_to_client(client_socket, whiteboard_id):
+    temp_file_path = f"temp_whiteboard_{whiteboard_id}.png"
 
     # Save the whiteboard image
+    whiteboard = whiteboards[whiteboard_id]
     whiteboard.save_picture_path(temp_file_path)
 
     with open(temp_file_path, "rb") as file:
@@ -58,27 +66,9 @@ def send_whiteboard_state_to_client(client_socket, whiteboard):
         # Send the image data to the client
         client_socket.sendall(image_data)
 
-    # Read the image file as binary data
-    '''with open(temp_file_path, "rb") as file:
-        chunk_size = 1024  # Set an appropriate chunk size
-        while True:
-            chunk = file.read(chunk_size)
-            if not chunk:
-                break
-            # Send each chunk with size information
-            send_data_with_size(client_socket, chunk)
-
-    # Send an empty chunk to indicate the end of data
-    send_data_with_size(client_socket, b"")'''
-
     # Remove the temporary file
-    os.remove(temp_file_path)
+    #os.remove(temp_file_path)
 
-def send_data_with_size(client_socket, data):
-    data_size = len(data)
-    size_data = struct.pack("!I", data_size)
-    client_socket.sendall(size_data)
-    client_socket.sendall(data)
 
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -86,15 +76,14 @@ def start_server():
     server.listen(5)
     print("Server listening on port 5555")
 
-    server_whiteboard = white_lib.WhiteboardApp()
-    server_whiteboard.initialize(False)
-
     while True:
         client, addr = server.accept()
         print(f"Accepted connection from {addr}")
-        client_handler = threading.Thread(target=handle_client, args=(client, addr, server_whiteboard))
+        client_handler = threading.Thread(target=handle_client, args=(client, addr))
         client_handler.start()
 
 
 if __name__ == "__main__":
+    whiteboards = {}  # Dictionary to store instances of WhiteboardApp
+
     start_server()
